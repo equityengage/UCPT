ecea26ded20b5cca1bdb36c634ba7e21638e62aa
EquityEngage <33866515+equityengage@users.noreply.github.com>
2017-11-21 07:21:46 -0500

    Create UCPT-Manager-Module.php

---
+++ UCPT-Manager-Module.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: UCPT Manager Module
+Plugin URI: http://code.mygarrettcounty.com
+Description: This plug-in is a UCPT module supporting UCPT driver plug-ins with additional variables
+Version: 0.9.1
+Requires at least: 3.3
+Tested up to: 4.8.1
+License: GPL v3
+Author: UCPT
+Author URI: http://code.mygarrettcounty.com
+*/
+//////////////////// Currently under development - install now to receive plug-in functionality upon completion.
+	
+?>
